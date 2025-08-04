ab41947540c028307aaacede693c3eb90a380900
SebastiaanK0508 <sebastiaankamphuis@outlook.com>
2025-08-04 22:09:15 +0200

    dsjfnhksjdfnskdjfhjhsd

--- achterkant/aanpassing/driver-details.php
+++ achterkant/aanpassing/driver-details.php
@@ -1,17 +1,5 @@
 <?php
-// Database configuratie
-$host = "localhost";
-$db = "formule1";
-$user = "root";
-$pass = "root";
-$charset = "utf8mb4";
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+require_once 'db_config.php';
 
 $pdo = null;
 $driverDetails = null; // Voor de details van de coureur
